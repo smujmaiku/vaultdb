ced6d53b32a5b33bec873d2e6fae7d4e3c060314
Michael Szmadzinski <smujmaiku@gmail.com>
2019-02-03 17:54:14 -0500

    init copy

---
+++ test/vaultdb.js
@@ -0,0 +1,10 @@
+const expect = require('expect');
+const VaultDb = require('../lib/vaultdb');
+
+describe('VaultDb', function() {
+	describe('something', () => {
+		it('should ', () => {
+			// expect('tests to be written').toBe(true);
+		});
+	});
+});
