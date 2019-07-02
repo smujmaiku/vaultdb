3a92ad2e87fab4fed1236d17006ab35076dca351
Michael Szmadzinski <smujmaiku@gmail.com>
2019-07-02 18:40:57 -0400

    beta warning

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log(`[smujmaiku/vaultdb] This is very beta, please don't use this in production just yet. ~Michael Szmadzinski`);
+
 exports = module.exports = require('./lib/vaultdb_basic');
 
 exports.VaultDb = require('./lib/vaultdb');
